683cabba2fa6eedfd99c2b5907fe7790be85b430
Adriaan van Schalkwyk <adriaan.vanschalkwyk@absa.africa>
2021-11-29 12:25:12 +0200

    Update Data+Decoding.swift
    update decoding

--- Dota 2 Heroes/Dota 2 Heroes/Extension/Data+Decoding.swift
+++ Dota 2 Heroes/Dota 2 Heroes/Extension/Data+Decoding.swift
@@ -8,7 +8,37 @@
 import Foundation
 
 public extension Data {
+    
     func decoded<T: Decodable>() throws -> T {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(DateFormatter.formatStandardDate)
+        do {
+            return try decoder.decode(T.self, from: self)
+        } catch let DecodingError.dataCorrupted(context) {
+            print(context)
+        } catch let DecodingError.keyNotFound(key, context) {
+            print("Key '\(key)' not found:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        } catch let DecodingError.valueNotFound(value, context) {
+            print("Value '\(value)' not found:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        } catch let DecodingError.typeMismatch(type, context) {
+            print("Type '\(type)' mismatch:", context.debugDescription)
+            print("codingPath:", context.codingPath)
+        } catch {
+            print("error: ", error)
+        }
+        
         return try JSONDecoder().decode(T.self, from: self)
     }
+}
+
+extension DateFormatter {
+    static let formatStandardDate: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
 }
